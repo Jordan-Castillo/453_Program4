3fdfced4529fceadcb19581d76621d57eacad993
Jordan-Castillo <jtcastil@calpoly.edu>
2018-05-21 16:58:17 -0700

    attempting to print superblock members v2

--- program4.c
+++ program4.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]){
       else{//start the work here....
          read_data(0, 0, p, 1024);
          superBlock = (struct ext2_super_block *)p;
-         printf("first byte... superBlock.s_inodes_count = '%d', also does this explode?", superBlock -> s_inodes_count);
+         printf("first byte... superBlock.s_inodes_count = '%u' or '%x', also does this explode?", superBlock -> s_inodes_count, superBlock -> s_inodes_count);
 
       }
    }
