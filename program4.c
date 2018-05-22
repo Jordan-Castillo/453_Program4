2e37bf4e53e649e0cee580c119a0a0b3ae841d40
Jordan-Castillo <jtcastil@calpoly.edu>
2018-05-21 19:29:55 -0700

    attempting to print superblock members v6

--- program4.c
+++ program4.c
@@ -1,39 +1,37 @@
 #include "program4.h"
 
+//GLOBALS...
 FILE *fp;
 
+//CONSTRUCTORS...
+void giveSuperBlock(struct ext2_super_block *p);
+
+
 int main(int argc, char *argv[]){
-   char *fileName;
-   struct ext2_super_block *supB;
-   uint8_t arr[1024];
-   uint8_t *p = &arr[0];
-   if(argc > 1){
-      fileName = argv[1];
-      fp = fopen(fileName, "r");
-      if(fp == NULL){
+   char *fileName, *destination;
+   struct ext2_super_block supB;
+   struct ext2_inode iNode;
+
+   fileName = argv[1];
+   if(argc > 1)
+      destination = argv[2];
+   else{
+      printf("Usage: ext2reader <flags> <filename> <directory>\n");
+      exit(0);
+   }
+   if((fp = fopen(fileName, "r")) == NULL){
          printf("cant find file '%s'!", fileName);
          exit(0);
-      }
-      else{//start the work here....
-         read_data(2, 0, p, 1024);
-         supB = (struct ext2_super_block *)p;
-         printf("form: %%x %%u %%d\n");
-         printf("sB.s_inodes_count = %x %u %d\n", supB -> s_inodes_count,
-         supB -> s_inodes_count, supB -> s_inodes_count);
-         printf("sB.s_blocks_count = %x %u %d\n", supB -> s_blocks_count,
-         supB -> s_blocks_count, supB -> s_blocks_count);
-         printf("sB.s_r_blocks_count = %x %u %d\n", supB -> s_r_blocks_count,
-         supB -> s_r_blocks_count, supB -> s_r_blocks_count);
-         printf("sB.s_free_blocks_count = %x %u %d\n", supB -> s_free_blocks_count,
-         supB -> s_free_blocks_count, supB -> s_free_blocks_count);
-         printf("sB.s_free_inodes_count = %x %u %d\n", supB -> s_free_inodes_count,
-         supB -> s_free_inodes_count, supB -> s_free_inodes_count);
-         printf("sB.s_log_block_size = %x %u %d\n", supB -> s_log_block_size,
-         supB -> s_log_block_size, supB -> s_log_block_size);
-      }
    }
-   else{ //error message
-      printf("Usage: ext2reader <flags> <filename> <directory>\n");
-   }
-   return 0;
+   giveSuperBlock(&supB);
+   printf("s_inodes_count = %u\n", supB.s_inodes_count);
+   printf("s_inodes_per_group = %u\n", supB.s_inodes_per_group);
+
+
+
+}
+
+void giveSuperBlock(struct ext2_super_block *p){
+   read_data(2, 0, (uint8_t*)p, sizeof(struct ext2_super_block));
+   return;
 }
